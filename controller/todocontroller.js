3c7c79a33ab09759c4eb3102674ceeea8ef10958
jayjariwala <jay.jariwala@hotmail.com>
2016-11-06 19:50:32 -0500

    form post commit

--- controller/todocontroller.js
+++ controller/todocontroller.js
@@ -1,15 +1,35 @@
-module.exports = function(app)
+
+
+
+module.exports = function(app,useragent)
 {
+
+  app.get('/',function(req,res){
+    var source=req.headers['user-agent'];
+
+    // ua has unique browser information
+    var ua=useragent.parse(source);
+
+    res.render('index');
+
+  });
+
 
 app.post('/todo',function(req,res){
 
+  console.log(req.body.task);
+  if(req.body.task=="")
+  {
+    console.log("blank");
+  }
 
 })
 
 app.post('/completed',function(req,res){
 
-  
+
 })
+
 
 
 }
--- server.js
+++ server.js
@@ -1,17 +1,23 @@
 var express=require('express');
 var app=express();
+var bodyParser=require('body-parser');
 var controller=require('./controller/todocontroller');
+var useragent=require('express-useragent');
 app.use(express.static('./public'));
-controller(app);
+
 
 // setup template engine
 app.set('view engine','ejs');
 
-app.get('/',function(req,res){
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
-  res.sendFile(__dirname+"/index.html");
+/**bodyParser.json(options)
+ * Parses the text as JSON and exposes the resulting object on req.body.
+ */
+app.use(bodyParser.json());
+controller(app,useragent);
 
-});
-
-var port = Number(process.env.PORT || 8080);
+var port = Number(process.env.PORT || 8081);
 app.listen(port);
