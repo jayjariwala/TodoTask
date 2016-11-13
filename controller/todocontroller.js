6b42674e6172d5f4ccb678f84c7760a6f6f33b41
jayjariwala <jay.jariwala@hotmail.com>
2016-11-13 18:58:37 -0500

    User task complete

--- controller/todocontroller.js
+++ controller/todocontroller.js
@@ -1,7 +1,8 @@
-
+var moment = require('moment');
 module.exports = function(app,useragent,mongoose,model)
 {
 
+  var updatedTask =[];
   var source;
   // ua has unique browser information
   var ua;
@@ -18,7 +19,19 @@ module.exports = function(app,useragent,mongoose,model)
     user.find({user_browser : ua.source,status:"unfinished"},function(err,task){
       if(err) throw err;
 
-      res.render('index',{incomplete:task});
+      for(var i=0;i<task.length;i++)
+      {
+        updatedTask[i] = {
+
+        user_browser:task[i].user_browser,
+        user_task:task[i].user_task,
+        status:task[i].status,
+        time:moment.unix(task[i].time).format("MMMM DD YYYY @ hh:mm A")
+        }
+
+      }
+
+      res.render('index',{incomplete:updatedTask});
 
     }).sort({'time':-1});
 
@@ -58,9 +71,8 @@ app.post('/todo',function(req,res){
     todotask.save(function(err){
       if(err) throw err;
       console.log("information stored successfully");
+      todotask.time=moment.unix(todotask.time).format("MMMM DD YYYY @ hh:mm A");
       res.json(todotask);
-
-
     })
 
 
--- public/js/request.js
+++ public/js/request.js
@@ -1,23 +1,40 @@
 $(document).ready(function(){
 
-  $('form').submit(function(){
+
+  $('#post').click(function(){
+    console.log("it is clicked");
+
     var taskdata=$('#task').val();
-    var formdata={ task: taskdata }
 
-    $.ajax({
-        type:"POST",
-        url:"/todo",
-        data: formdata,
-        success: function(data)
-        {
-          console.log("I have "+data);
-        },
-        error: function(err)
-        {
-          console.log("something went wrong");
-        }
+    if(taskdata == "")
+    {
+      console.log("do something");
+    }
+    else
+    {
+      var formdata={ task: taskdata }
 
-      })
+      $.ajax({
+          type:"POST",
+          url:"/todo",
+          data: formdata,
+          success: function(data)
+          {
+            $('.currenttask').prepend("<div class='tasks'><span class='check'><input type='checkbox' class='check'  id='checkbox_id' value='value'/></span><span class='text'><span class='title'><label for='checkbox_id' class='title'>"+data.user_task+"</label></span><br/><span class='time'>"+data.time+"</span></span></div>")
+            $('#task').val('')
+          },
+          error: function(err)
+          {
+            console.log("something went wrong");
+          }
+
+        })
+
+
+    }
+
+
   })
+
 
 })
